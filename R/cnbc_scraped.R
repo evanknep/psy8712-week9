6df8e8532aa182710b382a7de77e78394effd35d
evanknep <knepx001@umn.edu>
2024-03-24 22:02:20 -0500

    got dynamic publication result finished

--- R/cnbc_scraped.R
+++ R/cnbc_scraped.R
@@ -27,13 +27,44 @@ for (url in urls) {
   lengths <- headlines %>%
     str_count("\\w+")
   sources <- rep(source, length(headlines))
+  sources
   all_headlines <- c(all_headlines, headlines)
   all_lengths <- c(all_lengths, lengths)
   all_sources <- c(all_sources, sources)
 }
 
-cnbc_tbl <- tibble(all_headlines, all_lengths, all_sources)
+### realized I created a list where each element is a list of 1. This was a problem for graphing of course, 
+### and was preventing me from factorizing the source column. I imagine there is a cleaner way to fix that within my for loop
+### but I am running a bit low on time so am doing it the lazy way below.
+cnbc_tbl <- tibble("headline" = all_headlines, "length" = all_lengths, "source" = all_sources)
+cnbc_tbl$source <- sapply(cnbc_tbl$source, `[`, 1)
+cnbc_tbl$headline <- sapply(cnbc_tbl$headline, `[`, 1)
+cnbc_tbl$length <- sapply(cnbc_tbl$length, `[`, 1)
 
+cnbc_tbl$source <- factor(cnbc_tbl$source)
+
+
+#Analysis
+anova_result <- aov(length~source, data = cnbc_tbl)
+
+dfd <- anova_result$df.residual
+p_value <- formatC(summary(anova_result)[[1]]$"Pr(>F)"[1], digits = 2)
+f_value <- formatC(summary(anova_result)[[1]]$"F value"[1], digits = 2)
+dfn <- summary(anova_result)[[1]]$"Df"[1]
+
+is_sig <- ifelse(p_value <= 0.05, "was", "was not")
+
+
+
+#Visualization
+cnbc_tbl %>%
+  ggplot(aes(x=source, y =length)) +
+  geom_boxplot()
+
+#Publication
+paste("The results of an ANOVA comparing lengths across sources was ", 
+      "r(",dfn, "," ,dfd,") ", "= ", str_remove(f_value, "^0+"),
+      " p = ", str_remove(p_value, "^0+"), '.', 'This test ', is_sig, " statistically significant", sep = "")
 
 
 
